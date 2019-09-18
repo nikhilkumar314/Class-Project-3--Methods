c543f1dc67215a6dd48509320f25eb0c9d1d5268
Nikhil Kumar <nikhilkumar@usf.edu>
2019-09-17 21:43:28 -0400

    Methods

--- Class Project 3- Methods/Program.cs
+++ Class Project 3- Methods/Program.cs
@@ -10,6 +10,15 @@ namespace Class_Project_3__Methods
     {
         static void Main(string[] args)
         {
+            Username(); //Calls the method we made below 
+        }
+
+        static void Username ()
+        {
+            //Asks user to enter their name
+            Console.Write("Please enter your name: ");
+            string input = Console.ReadLine(); //Reads what the user entered plus stores it as a string value called input
+            Console.WriteLine("Hello" + " " + input + "!");
         }
     }
 }
